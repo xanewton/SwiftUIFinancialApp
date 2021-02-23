177dd60db94e5b1dba12325618102670e8b466e6
Angel Newton <xanewton@gmail.com>
2021-02-22 19:15:05 -0500

    Initial Commit

---
+++ SwiftUIFinancialApp/SwiftUIFinancialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFinancialAppApp.swift
+//  SwiftUIFinancialApp
+//
+//  Created by newtan on 2021-02-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFinancialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
