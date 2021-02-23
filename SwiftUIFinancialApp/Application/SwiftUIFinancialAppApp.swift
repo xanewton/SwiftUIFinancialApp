7f1fcb155b146912b3618c87b4445d317eb045f7
Angel Newton <xanewton@gmail.com>
2021-02-22 19:32:43 -0500

    Copy starter proyect

--- SwiftUIFinancialApp/Application/SwiftUIFinancialAppApp.swift
+++ SwiftUIFinancialApp/Application/SwiftUIFinancialAppApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct SwiftUIFinancialAppApp: App {
+    @StateObject private var model = CreateAccountViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(model)
         }
     }
 }
