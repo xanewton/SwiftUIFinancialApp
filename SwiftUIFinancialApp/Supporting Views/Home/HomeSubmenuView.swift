8bd8469054dac2498e9b8e88ec68a297440e8f7a
Angel Newton <xanewton@gmail.com>
2021-02-22 20:04:17 -0500

    Create HomeSubmenuView

--- SwiftUIFinancialApp/Supporting Views/Home/HomeSubmenuView.swift
+++ SwiftUIFinancialApp/Supporting Views/Home/HomeSubmenuView.swift
@@ -10,12 +10,83 @@ import SwiftUI
 
 struct HomeSubmenuView: View {
     var body: some View {
-        Text("Home Submenu View")
+        VStack(spacing: 0) {
+            HStack {
+                Text("QUICK SERVICES")
+                    .customFont(.custom(.bold, 20))
+                    .padding(.bottom, 2)
+                    .foregroundColor(.basePrussianBlue)
+                Spacer()
+            }
+
+            Divider().padding(.bottom, 10)
+
+            HStack {
+                Button(action: {
+
+                }) {
+                    VStack {
+                        Image("icon-send")
+                        Text("ADD NEW").customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+
+                Spacer()
+
+                Button(action: {
+
+                }) {
+                    VStack {
+                        Image("icon-receive")
+                        Text("CARDS").customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+
+
+                Spacer()
+
+                Button(action: {
+
+                }) {
+                    VStack {
+                        Image("icon-invoices")
+                        Text("LOCATIONS").customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+
+                Spacer()
+
+                Button(action: {
+
+                }) {
+                    VStack {
+                        Image("icon-bills")
+                        Text("TRANSACTIONS").customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+            }
+            .foregroundColor(.basePrussianBlue)
+            .padding(10)
+        }
+        .background(Color.baseWhite)
     }
 }
 
 struct HomeSubmenuView_Previews: PreviewProvider {
     static var previews: some View {
         HomeSubmenuView()
+            .previewLayout(.fixed(width: 600, height: 140))
     }
 }
