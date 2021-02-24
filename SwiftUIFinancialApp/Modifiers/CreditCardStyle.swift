b079aafc6e1a3f578f8c6ddb52817def1ff56a6c
Angel Newton <xanewton@gmail.com>
2021-02-23 19:36:27 -0500

    Create Credit card type menu

--- SwiftUIFinancialApp/Modifiers/CreditCardStyle.swift
+++ SwiftUIFinancialApp/Modifiers/CreditCardStyle.swift
@@ -7,3 +7,13 @@
 //
 
 import SwiftUI
+
+struct CreditCardStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(width: 75, height: 80)
+            .background(Color.white)
+            .cornerRadius(10)
+            .buttonStyle(PlainButtonStyle())
+    }
+}
--- SwiftUIFinancialApp/Supporting Views/Create Account/CreditCardTypeMenuView.swift
+++ SwiftUIFinancialApp/Supporting Views/Create Account/CreditCardTypeMenuView.swift
@@ -9,13 +9,36 @@
 import SwiftUI
 
 struct CreditCardTypeMenuView: View {
+    let logos = ["mc-logo-selector", "visa-logo-selector", "am-logo-selector"]
+
     var body: some View {
-        Text("Credit Card Type Menu View")
+        VStack {
+            VStack(alignment: .leading, spacing: 3) {
+                Text("SELECT A CARD TYPE")
+                    .customFont(.custom(.bold, 18))
+                    .foregroundColor(.basePrussianBlue)
+                    .padding(.leading, 10)
+                Divider()
+            }.padding(.top, 15)
+            
+            HStack {
+                ForEach(0..<logos.count) { index in
+                    Button(action:{ }) {
+                        VStack {
+                            Image(self.logos[index])
+                            Image("checkmark-outline-selector")
+                        }
+                    }
+                    .buttonStyle(CreditCardStyle())
+                }
+            }.padding(.top, 20)
+        }.background(Color.clear)
     }
 }
 
 struct CreditCardTypeMenuView_Previews: PreviewProvider {
     static var previews: some View {
         CreditCardTypeMenuView()
+            .previewLayout(.fixed(width: 400, height: 180))
     }
 }
