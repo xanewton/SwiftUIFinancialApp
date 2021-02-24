bf342543a02f1f025266435f181f1d749b7419c9
Angel Newton <xanewton@gmail.com>
2021-02-23 19:23:42 -0500

    Create AccountTypeView

--- SwiftUIFinancialApp/Supporting Views/Misc/AccountTypeView.swift
+++ SwiftUIFinancialApp/Supporting Views/Misc/AccountTypeView.swift
@@ -11,7 +11,28 @@ import SwiftUI
 
 struct AccountTypeView: View {
     var body: some View {
-        Text("Account Type View")
+        HStack(spacing: 15) {
+            ForEach(0..<2) { index in
+                Button(action: {}) {
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color.basePrussianBlue)
+                                .frame(width: 18, height: 18)
+                            Image("checkmark-selector")
+                                .resizable()
+                                .renderingMode(.template)
+                                .frame(width: 10, height: 8)
+                                .foregroundColor(.white)
+                        }
+                        Text("Account Type")
+                            .customFont(.custom(.medium, 14))
+                            .foregroundColor(.baseDustyGray)
+                    }
+                }.buttonStyle(PlainButtonStyle())
+            }
+            Spacer()
+        }
     }
 }
 
