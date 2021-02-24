d1dd6c3dd422bbcf527021b050351e72575ef69f
Angel Newton <xanewton@gmail.com>
2021-02-23 19:25:06 -0500

    Create ColorButtonMenu

--- SwiftUIFinancialApp/Supporting Views/Create Account/ColorButtonMenu.swift
+++ SwiftUIFinancialApp/Supporting Views/Create Account/ColorButtonMenu.swift
@@ -9,13 +9,35 @@
 import SwiftUI
 
 struct ColorButtonMenu: View {
+    private var selectedSegment: Int = 0
+    private var selectedColor: Color = .baseEndeavourBlue
+    private var colors: [Color] = [.baseEndeavourBlue,
+                                   .orange, .black, .red, .green, .purple, .gray]
+
     var body: some View {
-        Text("Color Button Menu")
+        HStack {
+            ForEach(0..<colors.count) { index in
+                Button(action: {
+                }) {
+                    ZStack {
+                        ColorView(color: self.colors[index])
+                        Image("checkmark-selector")
+                            .resizable()
+                            .renderingMode(.template)
+                            .opacity(self.selectedSegment == index ? 1 : 0)
+                            .frame(width: 12.0, height: 10.0)
+                            .foregroundColor(.white)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+        }
     }
 }
 
 struct ColorButtonMenu_Previews: PreviewProvider {
     static var previews: some View {
         ColorButtonMenu()
+            .previewLayout(.fixed(width: 300, height: 50))
     }
 }
